f1d0122b2cf0ba3a18b98de3d489f567116a0109
mirali <mirali2719@gmail.com>
2022-07-19 14:35:37 +0530

    changed greeting message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 //this is the route handler
 app.get('/',(req,res) => {
-    res.send({hi: 'there'});
+    res.send({bye: 'buddy'});
 });
 //dynamic port binding setup here
 const PORT = process.env.PORT || 5000;
